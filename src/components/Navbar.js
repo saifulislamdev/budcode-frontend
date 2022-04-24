5c82b94d74ac4f24f002aedfecf474c4ccba4833
Tufayel Ahmed <Tufayel5@gmail.com>
2022-04-23 23:57:54 -0400

    Profile Page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ const Navbar = () => {
     }
   };
 
-  
+  const username = JSON.parse(window.localStorage.getItem('username'));
 
   return (
 
@@ -55,7 +55,7 @@ const Navbar = () => {
             <a href="/">Home</a>
           </li>
           <li>
-            <a href="/users/:id">Profile</a>
+            <a href={`/users/${username}`}>Profile</a>
           </li>
           <li>
             <a href="/feed">Feed</a>
