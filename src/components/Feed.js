25849847119e908c65a05b58ebefef14243a075d
Anvinhtr <anvinhtr@gmail.com>
2022-05-21 23:43:39 -0400

    feed,profile

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -156,7 +156,8 @@ export default function Feed() {
                     <h4>{update.body}</h4>
                   </div>
                   <div className='custom-card-footer'>
-                    <p>{update.time_posted.slice(0,10)}</p>
+                 
+                    <p>{update.time_posted.slice(0,10)} {update.time_posted.slice(11,19)}</p>
                   </div>
                 </div>
               );
